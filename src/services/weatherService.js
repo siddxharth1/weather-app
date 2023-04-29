acc525a08bfe1156d56a737d3d68524d41091f83
Siddharth Sharma <vimalkumar.sddharth2021@vitbhopal.ac.in>
2023-04-29 22:11:32 +0530

    get city code from accuWeather api

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,5 +1,7 @@
 const API_KEY = "c4be0c8811c9d94722b0f8cb46eb7f4e"
 const Base_URL = "https://api.openweathermap.org/data/2.5"
+const accuWeatherApiKey = 'IivKY50fPmWgbjJ3TdCRgA5ylH1YZlR2';
+const citySearchBaseURL = 'http://dataservice.accuweather.com/locations/v1/cities/search'; //for city search
 
 const getWeatherData = (infoType, searchParams) =>{
     const url = new URL(Base_URL+ "/" + infoType)
@@ -7,13 +9,25 @@ const getWeatherData = (infoType, searchParams) =>{
     return fetch(url).then((resp)=>resp.json());
 }
 
+const getLocationKey = async (searchParams) => {
+    const cityURL = new URL(citySearchBaseURL);
+    cityURL.search = new URLSearchParams({apikey: accuWeatherApiKey, ...searchParams})
+    const cityResponse = await fetch(cityURL).then((resp)=>resp.json())
+    return cityResponse[0];
+}
+
+// const getForcastData = (searchParams) => {  
+//     const forcastURL = new URL()
+//     forcastURL.search = new URLSearchParams({apikey: accuWeatherApiKey, ...searchParams})
+// }
+
 const formatCurrentData =(data)=>{
     const {
         coord:{lat, lon},
         main:{temp, feels_like, temp_min, temp_max, pressure, humidity},
         name,
         weather,
-        wind:{speed}, 
+        wind:{speed},
         dt,
         sys:{country, sunrise, sunset},
         cod
@@ -27,6 +41,9 @@ const formatCurrentData =(data)=>{
 const getFormattedWeatherData = async (searchParams)=>{
     const formattedWeatherData = await getWeatherData('weather', searchParams).then(data=>formatCurrentData(data));
     // const {lat, lon} = formatCurrentData;
+    const locationKey = await getLocationKey(searchParams).then(locKey => locKey.Key)
+    // const locationKey = await getLocationKey(searchParams).then(locKey => {return getForcastData(locKey.key)})
+    console.log(locationKey)
     
     return formattedWeatherData;
 }
