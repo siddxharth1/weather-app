6296a95f40a7735db19219480435c6b2325e31d1
Siddharth Sharma <vimalkumar.sddharth2021@vitbhopal.ac.in>
2023-05-04 11:48:55 +0530

    luxon installed

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -15,9 +15,10 @@ const hourlyForcastURL = 'http://dataservice.accuweather.com/forecasts/v1/hourly
 //returning current time date according to timezone(searchParams)(kolkata/india)
 const getTime = async (searchParams) => {
     const url = new URL(timeBaseURL);
-    url.search = new URLSearchParams({ apiKey: timeAPI, ...searchParams })
-    const time = await fetch(url).then((resp) => resp.json());
-    return time.date_time_txt
+    url.search = new URLSearchParams({ apiKey: timeAPI, ...searchParams})
+    var time = await fetch(url).then((resp) => resp.json());
+    const {date_time_txt, date_time_unix, time_12} = time
+    return {date_time_txt, date_time_unix, time_12}
 }
 
 //returning all the current weather data openWeatherAPI
@@ -33,13 +34,12 @@ const getLocationKey = async (searchParams) => {
     cityURL.search = new URLSearchParams({ apikey: accuWeatherApiKey, ...searchParams })
     const cityResponse = await fetch(cityURL).then((resp) => resp.json())
     const cityRequired = cityResponse[0] //we will get all the city name matches our search so we will pick the first one which is most relevent and used
-
     // console.log(cityRequired)
 
     //destructuring key and timezone
     function getIdZone(data) {
         const { Key, TimeZone: { Name }, EnglishName: cityName, AdministrativeArea: { LocalizedName: stateName }, Country: { EnglishName: countryName } } = data
-        return { Key, Name, stateName, cityName, countryName }
+        return { Key, Name, cityName, stateName, countryName }
     }
     const keyName = getIdZone(cityRequired) //storing key and name in keyName variable
     return keyName;
@@ -48,19 +48,50 @@ const getLocationKey = async (searchParams) => {
 //returning daily forcast data
 const getDailyForcastData = async (searchParams) => {
     const dailyForcastDataURL = new URL(dailyForcastURL + searchParams)
-    dailyForcastDataURL.search = new URLSearchParams({ apikey: accuWeatherApiKey })
+    dailyForcastDataURL.search = new URLSearchParams({ apikey: accuWeatherApiKey, metric:true})
     const forcastResponse = await fetch(dailyForcastDataURL).then((resp) => resp.json())
-    // console.log(forcastResponse)
-    return forcastResponse.DailyForecasts;
+    let dailyData = forcastResponse.DailyForecasts
+
+    const formatDailyForcastData = (dailyData)=>{
+        dailyData = dailyData.map((d)=>{
+            return{
+                info: d.Day.IconPhrase,
+                icon: d.Day.Icon,
+                date: d.Date,
+                time: d.EpochDate,
+                max_temp: d.Temperature.Maximum.Value,
+                min_temp: d.Temperature.Minimum.Value
+            }
+        })
+        return(dailyData)
+    }
+    let dailyForcastMain = formatDailyForcastData(dailyData);
+    // console.log(dailyForcastMain)
+    return dailyForcastMain;
 }
 
 //returning hourly forcast data
 const getHourlyForcastData = async (searchParams) => {
     const hourlyForcastDataURL = new URL(hourlyForcastURL + searchParams)
-    hourlyForcastDataURL.search = new URLSearchParams({ apikey: accuWeatherApiKey })
-    const hourlyForcastResponse = await fetch(hourlyForcastDataURL).then((resp) => resp.json())
+    hourlyForcastDataURL.search = new URLSearchParams({ apikey: accuWeatherApiKey, metric:true })
+    let hourlyForcastResponse = await fetch(hourlyForcastDataURL).then((resp) => resp.json())
     // console.log(hourlyForcastResponse);
-    return hourlyForcastResponse;
+
+    //getting only required data 
+    const formatHourlyForcastData = (hourlyForcastResponse)=>{
+        hourlyForcastResponse = hourlyForcastResponse.slice(1,6).map((h)=>{
+            return {
+                temp: h.Temperature.Value,
+                icon: h.WeatherIcon,
+                title: h.EpochDateTime,
+                info: h.IconPhrase
+            }
+        })
+        return(hourlyForcastResponse)
+    }
+    let hourlyForcastMain = formatHourlyForcastData(hourlyForcastResponse);
+    // console.log(hourlyForcastMain)
+    return hourlyForcastMain;
 }
 
 //formatting(destructuring) all the current data and returning all the required data as an object
