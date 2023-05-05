21d76eb06ae14b16fb2ad1d763fb8767dc8e090f
Siddharth Sharma <vimalkumar.sddharth2021@vitbhopal.ac.in>
2023-05-05 21:13:52 +0530

    update DataBody.js

--- src/components/DataBody.js
+++ src/components/DataBody.js
@@ -3,19 +3,33 @@ import './DataBody.css'
 import Weather from './Weather'
 import DayWeekForcast from './DayWeekForcast'
 
-function DataBody(weather) {
+
+
+function DataBody({weatherData}) {
+  const greetingFn= ()=>{
+    let hour = weatherData.date_time.time_hour
+    if (hour >= 5 && hour < 12) {
+      return('Good morning');
+    } else if (hour >= 12 && hour < 18) {
+      return('Good afternoon');
+    } else {
+      return('Good evening');
+    }
+  }
+  const greetingMsg = greetingFn()
+
   return (
     <div className='weather-main'>
       <div className='heading'>
         <div className='greating'>
-          Good Morning
+          {greetingMsg}
         </div>
-        <div className='date-time'>Tuesday, 31 May'22 | 12:46:20 PM</div>
+        <div className='date-time'>{weatherData.date_time.displayTime}</div>
       </div>
 
       <div className='weather-info'>
-        <Weather/>
-        <DayWeekForcast/>
+        <Weather currentWeather={weatherData}/>
+        <DayWeekForcast forcastWeather={weatherData}/>
       </div>
 
     </div>
