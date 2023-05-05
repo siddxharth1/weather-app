791917db11224cd9cec3f3a765f979ee7a479905
Siddharth Sharma <vimalkumar.sddharth2021@vitbhopal.ac.in>
2023-05-05 23:00:37 +0530

    update DataBody.css and DayWeekForcast.js

--- src/components/DayWeekForcast.js
+++ src/components/DayWeekForcast.js
@@ -5,7 +5,6 @@ import './DayWeekForcast.css'
 function DayWeekForcast({forcastWeather}) {
   return (
     <div className='forcast-main'>
-      {/* <HourlyForcast hourlyForcast={forcastWeather.hourly}/> */}
       <div>
             <div className='forcast-heading'>
                 <p>Hourly Forcast</p>
@@ -15,7 +14,7 @@ function DayWeekForcast({forcastWeather}) {
         </div>
       <br />
       <br />
-      {/* <DailyForcast dailyForcast={forcastWeather.daily}/> */}
+      
       <div>
             <div className='forcast-heading'>
                 <p>Daily Forcast</p>
