88176f3db97eb17a53ab8da6808a9adce61abee4
Siddharth Sharma <vimalkumar.sddharth2021@vitbhopal.ac.in>
2023-05-05 11:45:22 +0530

    appjs

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import './App.css';
 import Navbar from './components/Navbar';
 import DataBody from './components/DataBody';
 import getFormattedWeatherData from './services/weatherService'
+import { useEffect, useState } from 'react';
 
 
-function App() {  
-  const fetchWeatherData = async()=>{
-    const wdata = await getFormattedWeatherData({q:"delhi", units: 'imperial' })
-    console.log(wdata)
+function App() {
+
+  const fetchWeatherData1 = async()=>{
+    const dataa = await getFormattedWeatherData({q:'delhi', units:'metric'})
+    console.log(dataa)
   }
-  fetchWeatherData()
+  fetchWeatherData1()
+
+  const [query, setQuery] = useState({ q: "delhi" })
+  const [units, setUnits] = useState('metric')
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      await getFormattedWeatherData({ query, units }).then((data)=>{
+        setWeather(data)
+      })
+    }
+    fetchWeatherData()
+  }, [query, units])
+
 
   return (
     <div className='main'>
-      <Navbar/>
-      <DataBody/>
+      <Navbar />
+      <DataBody weather={weather} />
     </div>
   );
 }
