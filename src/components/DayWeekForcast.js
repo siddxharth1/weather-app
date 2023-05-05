90b74f45aa3727a70b56daa4eaf9a23a9288faa9
Siddharth Sharma <vimalkumar.sddharth2021@vitbhopal.ac.in>
2023-05-05 22:57:39 +0530

    delete 2 files and update 1 file

--- src/components/DayWeekForcast.js
+++ src/components/DayWeekForcast.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import HourlyForcast from './HourlyForcast'
-import DailyForcast from './DailyForcast'
+import ForcastData from './ForcastData'
+import './DayWeekForcast.css'
 
 function DayWeekForcast({forcastWeather}) {
   return (
     <div className='forcast-main'>
-      <HourlyForcast hourlyForcast={forcastWeather.hourly}/>
+      {/* <HourlyForcast hourlyForcast={forcastWeather.hourly}/> */}
+      <div>
+            <div className='forcast-heading'>
+                <p>Hourly Forcast</p>
+            </div>
+            <hr />
+            <ForcastData/>
+        </div>
       <br />
       <br />
-      <DailyForcast dailyForcast={forcastWeather.daily}/>
+      {/* <DailyForcast dailyForcast={forcastWeather.daily}/> */}
+      <div>
+            <div className='forcast-heading'>
+                <p>Daily Forcast</p>
+            </div>
+            <hr />
+            <ForcastData/>
+        </div>
     </div>
   )
 }
