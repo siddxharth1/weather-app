b731b8bb63d3b410f8e217c82e7a2108c24b7e71
Siddharth Sharma <vimalkumar.sddharth2021@vitbhopal.ac.in>
2023-05-05 22:54:58 +0530

    update DailyForcast.js

--- src/components/DailyForcast.js
+++ src/components/DailyForcast.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './DayWeekForcast.css'
+import ForcastData from './ForcastData'
 
 function DailyForcast() {
     return (
@@ -8,34 +9,7 @@ function DailyForcast() {
                 <p>Daily Forcast</p>
             </div>
             <hr />
-
-            <div className='hourly-forcast-main'>
-                <div className='hourly-forcast'>
-                    <p>Wed</p>
-                    <img className='forcast-image' src={(require('./images/sunny.svg')).default} alt=''/>
-                    <p>21&deg;</p>
-                </div>
-                <div className='hourly-forcast'>
-                    <p>Thu</p>
-                    <img className='forcast-image' src={(require('./images/sunny.svg')).default} alt=''/>
-                    <p>21&deg;</p>
-                </div>
-                <div className='hourly-forcast'>
-                    <p>Fri</p>
-                    <img className='forcast-image' src={(require('./images/sunny.svg')).default} alt=''/>
-                    <p>21&deg;</p>
-                </div>
-                <div className='hourly-forcast'>
-                    <p>Sat</p>
-                    <img className='forcast-image' src={(require('./images/sunny.svg')).default} alt=''/>
-                    <p>21&deg;</p>
-                </div>
-                <div className='hourly-forcast'>
-                    <p>Sun</p>
-                    <img className='forcast-image' src={(require('./images/sunny.svg')).default} alt=''/>
-                    <p>21&deg;</p>
-                </div>
-            </div>
+            <ForcastData/>
         </div>
     )
 }
