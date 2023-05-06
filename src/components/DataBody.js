6599137e3fefd03f5448aebb47ff047cb547f015
Siddharth Sharma <vimalkumar.sddharth2021@vitbhopal.ac.in>
2023-05-06 18:50:04 +0530

    update DataBody.js

--- src/components/DataBody.js
+++ src/components/DataBody.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './css/DataBody.css'
 import Weather from './Weather'
 import './css/DayWeekForcast.css'
 import ForcastData from './ForcastData'
+import {getTime} from '../services/weatherService'
+// import {formatToLocalTime} from '../services/weatherService'
+import {DateTime} from 'luxon'
 
 function DataBody({ weatherData }) {
+  const [time, setTime] =useState()
+  const formatToLocalTime = (secs, zone, format)=>DateTime.fromSeconds(secs).setZone(zone).toFormat(format)
+  const timeZone = weatherData.locInfo.Name
+
+  const getTimee = async()=>{
+    const unixSecs = await getTime(timeZone)
+    let timeee = formatToLocalTime(unixSecs.timeSecs, timeZone,"cccc, dd LLL yyyy' |' hh:mm:ss a")
+    setTime(timeee)
+    return timeee
+  }
+  setInterval(() => {
+    getTimee()
+  }, 800);
+  
+
   const greetingFn = () => {
     let hour = weatherData.date_time.time_hour
     if (hour >= 5 && hour < 12) {
@@ -23,7 +41,8 @@ function DataBody({ weatherData }) {
         <div className='greating'>
           {greetingMsg}
         </div>
-        <div className='date-time'>{weatherData.date_time.displayTime}</div>
+        {/* <div className='date-time'> {weatherData.date_time.displayTime}</div> */}
+        <div className='date-time'>{time} </div>
       </div>
 
       <div className='weather-info'>
