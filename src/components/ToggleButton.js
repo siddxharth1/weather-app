8eb6fa02800f438e62d6cd4425b6676ca3d816b4
Siddharth Sharma <vimalkumar.sddharth2021@vitbhopal.ac.in>
2023-05-07 09:09:00 +0530

    feat: create ToggleButton.js

---
+++ src/components/ToggleButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ToggleButton() {
+  return (
+    <div>
+      <input type="radio" name="unit" id="yes" />
+      <input type="radio" name="unit" id="no" />
+    </div>
+  )
+}
+
+export default ToggleButton
