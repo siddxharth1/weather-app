efdb2cbef4cb6fd96e7542070358f325c4ae57b1
Siddharth Sharma <vimalkumar.sddharth2021@vitbhopal.ac.in>
2023-05-08 12:22:11 +0530

    update images.js

--- src/components/images.js
+++ src/components/images.js
@@ -1,4 +1,22 @@
 const images = {
-    sunny : require('./images/sunny.svg').default
+    '01d' : require('./images/weather_icons/clear_day.svg').default,
+    '01n' : require('./images/weather_icons/clear_night.svg').default,
+    '02d' : require('./images/weather_icons/few_clouds_day.svg').default,
+    '02n' : require('./images/weather_icons/few_clouds_night.svg').default,
+    '03d' : require('./images/weather_icons/scattered_clouds_day_night.svg').default,
+    '03n' : require('./images/weather_icons/scattered_clouds_day_night.svg').default,
+    '04d' : require('./images/weather_icons/broken_clouds_day_night.svg').default,
+    '04n' : require('./images/weather_icons/broken_clouds_day_night.svg').default,
+    '09d' : require('./images/weather_icons/shower_rain_day_night.svg').default,
+    '09n' : require('./images/weather_icons/shower_rain_day_night.svg').default,
+    '10d' : require('./images/weather_icons/rain_day.svg').default,
+    '10n' : require('./images/weather_icons/rain_night.svg').default,
+    '11d' : require('./images/weather_icons/thunderstorm_day_night.svg').default,
+    '11n' : require('./images/weather_icons/thunderstorm_day_night.svg').default,
+    '13d' : require('./images/weather_icons/snow_day_night.svg').default,
+    '13n' : require('./images/weather_icons/snow_day_night.svg').default,
+    '50d' : require('./images/weather_icons/mist_day_night.svg').default,
+    '50n' : require('./images/weather_icons/mist_day_night.svg').default,
+    sunny : require('./images/sunny.svg').default,
 }
 export default images;
