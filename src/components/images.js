4fccd5e343755a319c76d132ee17ea2f32c7898b
Siddharth Sharma <vimalkumar.sddharth2021@vitbhopal.ac.in>
2023-05-09 11:36:27 +0530

    update images.js

--- src/components/images.js
+++ src/components/images.js
@@ -13,17 +13,33 @@ const images = {
     '5' : require('./images/weather_icons/hazy_sunshine.svg').default,
     '6' : require('./images/weather_icons/mostly_cloudy.svg').default,
     '7' : require('./images/weather_icons/scattered_clouds_day_night.svg').default,
+    '8' : require('./images/weather_icons/dreary.svg').default,
+    '11' : require('./images/weather_icons/fog.svg').default,
 
     '03d' : require('./images/weather_icons/scattered_clouds_day_night.svg').default,
     '03n' : require('./images/weather_icons/scattered_clouds_day_night.svg').default,
     '04d' : require('./images/weather_icons/broken_clouds_day_night.svg').default,
     '04n' : require('./images/weather_icons/broken_clouds_day_night.svg').default,
+
     '09d' : require('./images/weather_icons/shower_rain_day_night.svg').default,
     '09n' : require('./images/weather_icons/shower_rain_day_night.svg').default,
+    '12' : require('./images/weather_icons/shower_rain_day_night.svg').default,
+
+    '13' : require('./images/weather_icons/mostly_cloudy_w_shower.svg').default,
+    '14' : require('./images/weather_icons/partially_sunny_w_showers.svg').default,
+    
     '10d' : require('./images/weather_icons/rain_day.svg').default,
     '10n' : require('./images/weather_icons/rain_night.svg').default,
+
     '11d' : require('./images/weather_icons/thunderstorm_day_night.svg').default,
     '11n' : require('./images/weather_icons/thunderstorm_day_night.svg').default,
+    '15' : require('./images/weather_icons/thunderstorm_day_night.svg').default,
+
+    '16' : require('./images/weather_icons/mostly_cloudy_w_tStorm.svg').default,
+    '17' : require('./images/weather_icons/partially_cloudy_w_tStorms.svg').default,
+    '18' : require('./images/weather_icons/rain.svg').default,
+    '19' : require('./images/weather_icons/flurries.svg').default,
+    
     '13d' : require('./images/weather_icons/snow_day_night.svg').default,
     '13n' : require('./images/weather_icons/snow_day_night.svg').default,
     '50d' : require('./images/weather_icons/mist_day_night.svg').default,
